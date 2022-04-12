90730e0c9aff030f43a47e21aebb5fb2a006cf0a
ricky80l <98959778+ricky80l@users.noreply.github.com>
2022-04-12 22:08:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+function dice() {
+    for (var n = 0; n < 5; n++) {
+        var pl1 = Math.floor(Math.random() * 6) + 1;
+    }
+    for (var n = 0; n < 5; n++) {
+        var pl2 = Math.floor(Math.random() * 6) + 1;
+    }
+
+    if (pl1 > pl2) {
+        document.querySelector("h1").textContent = "Player 1 wins over Player 2";
+    } else if (pl1 < pl2) {
+        document.querySelector("h1").textContent = "Player 2 wins over Player 1";
+    } else if (pl1 === pl2) {
+        document.querySelector("h1").textContent = "Player 1 and Player 2 tie";
+    }
+
+    // Dice One
+    if (pl1 === 1) {
+        document.querySelector("#p1 .centro").style.visibility = "visible";
+    }
+    if (pl1 === 2) {
+        document.querySelector("#p1 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p1 .dx_top").style.visibility = "visible";
+    }
+    if (pl1 === 3) {
+        document.querySelector("#p1 .centro").style.visibility = "visible";
+        document.querySelector("#p1 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p1 .dx_top").style.visibility = "visible";
+    }
+    if (pl1 === 4) {
+        document.querySelector("#p1 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p1 .sx_top").style.visibility = "visible";
+        document.querySelector("#p1 .dx_top").style.visibility = "visible";
+        document.querySelector("#p1 .dx_bot").style.visibility = "visible";
+    }
+    if (pl1 === 5) {
+        document.querySelector("#p1 .centro").style.visibility = "visible";
+        document.querySelector("#p1 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p1 .sx_top").style.visibility = "visible";
+        document.querySelector("#p1 .dx_top").style.visibility = "visible";
+        document.querySelector("#p1 .dx_bot").style.visibility = "visible";
+    }
+    if (pl1 === 6) {
+        document.querySelector("#p1 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p1 .sx_top").style.visibility = "visible";
+        document.querySelector("#p1 .dx_top").style.visibility = "visible";
+        document.querySelector("#p1 .dx_bot").style.visibility = "visible";
+        document.querySelector("#p1 .sx_md").style.visibility = "visible";
+        document.querySelector("#p1 .dx_md").style.visibility = "visible";
+    }
+
+    // Dice Two
+    if (pl2 === 1) {
+        document.querySelector("#p2 .centro").style.visibility = "visible";
+    }
+    if (pl2 === 2) {
+        document.querySelector("#p2 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p2 .dx_top").style.visibility = "visible";
+    }
+    if (pl2 === 3) {
+        document.querySelector("#p2 .centro").style.visibility = "visible";
+        document.querySelector("#p2 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p2 .dx_top").style.visibility = "visible";
+    }
+    if (pl2 === 4) {
+        document.querySelector("#p2 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p2 .sx_top").style.visibility = "visible";
+        document.querySelector("#p2 .dx_top").style.visibility = "visible";
+        document.querySelector("#p2 .dx_bot").style.visibility = "visible";
+    }
+    if (pl2 === 5) {
+        document.querySelector("#p2 .centro").style.visibility = "visible";
+        document.querySelector("#p2 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p2 .sx_top").style.visibility = "visible";
+        document.querySelector("#p2 .dx_top").style.visibility = "visible";
+        document.querySelector("#p2 .dx_bot").style.visibility = "visible";
+    }
+    if (pl2 === 6) {
+        document.querySelector("#p2 .sx_bot").style.visibility = "visible";
+        document.querySelector("#p2 .sx_top").style.visibility = "visible";
+        document.querySelector("#p2 .dx_top").style.visibility = "visible";
+        document.querySelector("#p2 .dx_bot").style.visibility = "visible";
+        document.querySelector("#p2 .sx_md").style.visibility = "visible";
+        document.querySelector("#p2 .dx_md").style.visibility = "visible";
+    }
+}
